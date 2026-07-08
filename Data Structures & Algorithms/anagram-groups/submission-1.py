585a39540f3ab5184232c32253eb34f799cfa833
Ahmed EL Tlawy <88274886+ahmedmohsenfawzy@users.noreply.github.com>
2026-07-08 21:28:51 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        dic = defaultdict(list)
+        for i in strs:
+            s = sorted(i)
+            dic[tuple(s)].append(i)
+        for i in dic.keys():
+            res.append(dic[i])
+        if not res:
+            res.append([""])
+        return res
+                
